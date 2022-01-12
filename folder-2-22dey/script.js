ba42230e76188f2af94814a70528f3f496c15737
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-01-12 21:10:55 +0330

    update

---
+++ folder-2-22dey/script.js
@@ -0,0 +1,76 @@
+
+
+// let allPrice = 0
+
+// for (let i = 0 ; i < 5 ; i++){
+//     allPrice = allPrice + Number(prompt("Please Enter the product price"))
+// }
+
+// alert("All price of your basket is : " + allPrice)
+
+
+
+
+// let userNumber = 0 // 12 // 14 // 18 // 22 // 40
+// let sum = 0 // 12 + 0 = 12 // 14 + 12 = 26 // 18 + 26 = 44 // 22 + 44 = 66 //  40 + 66 = 106 // 106 / 5 = 21.2
+// for (let i = 0 ; i < 5 ; i++){
+//     userNumber = Number(prompt("Enter the " + (i + 1) +" number : "))
+//     sum = sum + userNumber
+// }
+
+// alert(sum / 5)
+
+
+
+// let userNumber = prompt("Enter your number")
+
+// let sum = 0
+//  // 152 = 1 + 5 + 2 = 8
+//  // useNumber = 152 // sum = 0 + 2 => 2 // userNumber = 15 //sum = 2 + (15 % 10 = 5) = 7 // userNumber = 1 //
+//  // sum = 7 + (1 % 10 = 1) = 8 // floor => 1 / 10 = 0 => exit 
+// for (let i = 0 ; (userNumber / 10 ) != 0 ;i++){
+//     sum = sum + (userNumber % 10) 
+//     userNumber = Math.floor(userNumber / 10)
+// }
+// alert(sum)
+
+
+////////////////////////////////////////////////////////////
+
+// array method 
+
+// let arr = ["I","love","javaScript"]
+
+// delete arr[2]
+// console.log(arr)
+
+// arr.push("javaScript")
+
+// console.log(arr)
+
+// arr.splice(1,1)
+
+// console.log(arr)
+
+// let arr = ["I","study","javaScript","right","now"]
+
+// arr.splice(0,3,"Lets","dance")
+
+// console.log(arr)
+
+
+// let arr = ["I","study","javaScript","right","now"]
+
+// let remove = arr.splice(0,2)
+
+// console.log(remove) // i , study
+
+
+// ["Bilbo", "Gandalf", "Nazgul"].forEach((a,b,c) => {
+//   console.log(`${a} is at index ${b + 1} in ${c}`);
+// });
+
+
+
+
+
