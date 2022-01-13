2ae6ac491a71febbc6e965810f3873642059f266
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-01-13 18:22:58 +0330

    update

---
+++ folder-3-23dey/script.js
@@ -0,0 +1,37 @@
+
+
+// let fisrt = Number(prompt("Enter the first one"))
+// let secound = Number(prompt("Enter the secound one"))
+
+
+// if(fisrt % 2 ===0){
+//     while(fisrt < secound){
+//         alert(fisrt)
+//         fisrt+=2
+//     }
+// }else{
+//     fisrt++
+//     while(fisrt < secound){
+//         alert(fisrt)
+//         fisrt+=2
+//     }
+// }
+
+
+
+// let userNumber = 0
+// let sum = 0
+// let counter = 0
+// while (userNumber != -1){
+//     sum = sum + userNumber
+//     userNumber = Number(prompt("Enter your number"))
+//     if(userNumber != -1){
+//      counter++
+//     }  
+// }
+
+// alert(sum / counter)
+
+
+
+
