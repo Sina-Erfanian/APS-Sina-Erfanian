63b68784d27dd4d9a3d02676bd7759d529508224
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-01-15 16:38:19 +0330

    loop

--- folder-4-25Dey/script.js
+++ folder-4-25Dey/script.js
@@ -1,28 +1,70 @@
-let i = 0
+// let i = 0
 
 
-do {
-    console.log(i)
-    i++
-} while(i > 1)
+// do {
+//     console.log(i)
+//     i++
+// } while(i > 1)
 
 // the do while loop first execute do then check the condition
 
 
 
 ////////////////////// For loop ///////////////////////////
-let userNumber1 = Number(prompt("Enter the first number")) //2
-let userNumber2 = Number(prompt("Enter the secound number")) //3
+// let userNumber1 = Number(prompt("Enter the first number")) //2
+// let userNumber2 = Number(prompt("Enter the secound number")) //3
 
-//0 < 3? true => power = 1 * 2 => power = 2
-//1 < 3? true => power = 2 * 2 => power = 4
-//2 < 3? true => power = 4 * 2 => power = 8
-// 3 < 3 ? false end loop and power is 8
+// //0 < 3? true => power = 1 * 2 => power = 2
+// //1 < 3? true => power = 2 * 2 => power = 4
+// //2 < 3? true => power = 4 * 2 => power = 8
+// // 3 < 3 ? false end loop and power is 8
 
-let power = 1
-for (let i = 0 ; i < userNumber2 ; i++) {
- power = power * userNumber1
-}
-alert("Power " + power)
+// let power = 1
+// for (let i = 0 ; i < userNumber2 ; i++) {
+//  power = power * userNumber1
+// }
+// alert("Power " + power)
+
+
+
+/////////////////////// while ///////////////////////////
+
+// let userNumber1 = Number(prompt("Enter the first number"))
+// let userNumber2 = Number(prompt("Enter the secound number"))
+
+// let i = 0
+
+// let power = 1
+// while (i < userNumber2){
+//     power = power * userNumber1
+//     i++
+// }
+
+// alert(power)
+
+
+
+////////////////////////do while ////////////////////////////////
+
+// let userNumber1 = Number(prompt("Enter the first number"));
+// let userNumber2 = Number(prompt("Enter the secound number"));
+
+// let i = 0;
+
+// let power = 1;
+
+// do {
+//     power = power * userNumber1
+//     i++
+// } while(i < userNumber2)
+
+// alert(power)
+
+
+
+
+
+
+
 
 
