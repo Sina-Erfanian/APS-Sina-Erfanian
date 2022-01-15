7593de35fe11b3482b96c98d7b919ac17505f3a0
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-01-15 16:33:58 +0330

    for loop

---
+++ folder-4-25Dey/script.js
@@ -0,0 +1,28 @@
+let i = 0
+
+
+do {
+    console.log(i)
+    i++
+} while(i > 1)
+
+// the do while loop first execute do then check the condition
+
+
+
+////////////////////// For loop ///////////////////////////
+let userNumber1 = Number(prompt("Enter the first number")) //2
+let userNumber2 = Number(prompt("Enter the secound number")) //3
+
+//0 < 3? true => power = 1 * 2 => power = 2
+//1 < 3? true => power = 2 * 2 => power = 4
+//2 < 3? true => power = 4 * 2 => power = 8
+// 3 < 3 ? false end loop and power is 8
+
+let power = 1
+for (let i = 0 ; i < userNumber2 ; i++) {
+ power = power * userNumber1
+}
+alert("Power " + power)
+
+
