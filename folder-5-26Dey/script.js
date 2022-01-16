1753a8d3d72d579de83ac2fff5286e7766abb7b0
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-01-16 14:51:18 +0330

    update

--- folder-5-26Dey/script.js
+++ folder-5-26Dey/script.js
@@ -14,3 +14,59 @@
 // sum += arr[i]
 // }
 // alert(sum / arr.length)
+
+
+
+
+
+
+
+let userInfo = [
+  {
+    id : 1,
+    name: "sina",
+    family: "erfanian",
+    age: 19,
+    email: "sina@gmail.com",
+  },
+  {
+    id : 2,
+    name: "sara",
+    family: "mohammadi",
+    age: 29,
+    email: "sara@gmail.com",
+  },
+  {
+    id : 3,
+    name: "ali",
+    family: "shakeri",
+    age: 16,
+    email: "ali@gmail.com",
+  }
+];
+
+
+
+let UserName = prompt("Enter your name")
+let UserFamily = prompt("Enter your family name")
+let UserAge = prompt("Enter your Age")
+let UserEmail = prompt("Enter your Email")
+
+
+if (UserName < 3 || UserName > 10){
+    alert("your username should contain at least 3 and maximum 10")
+} else if(UserFamily < 3 || UserFamily > 15){
+    alert("your family name should contain at least 3 and maximum 15")
+} else if (isNaN(UserAge) || UserAge.length > 3){
+    alert("please Enter your age corectly")
+} else{
+    let newUser = {
+        id : 4,
+        name : UserName,
+        family : UserFamily,
+        age : UserAge,
+        email : UserEmail
+    }
+    userInfo.push(newUser)
+    console.log(userInfo)
+}
