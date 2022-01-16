2c5e9868f4cd95ea0a9b92e025be9b3578a5d230
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-01-16 13:08:56 +0330

    Update script.js

---
+++ folder-5-26Dey/script.js
@@ -0,0 +1,16 @@
+
+// let arr = [] // [12,14,16] len = 3 // arr[0]=  12 // arr[1] = 14 // arr[2]=16
+// let sum = 0 // 12 + 0 =12 // 14 + 12 = 26 // 16 + 26 = 42 // 42 / 3
+// let userNumber = 0
+
+// while(userNumber != -1){
+//     userNumber = Number(prompt("Enter your number \n press -1 if dont want to continue"))
+//     if (userNumber != -1){
+//         arr.push(userNumber)
+//     }
+// }
+
+// for (let i = 0; i < arr.length ; i++){
+// sum += arr[i]
+// }
+// alert(sum / arr.length)
