490514ac7a81a78c316a4dac727f942be2db324d
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-01-18 15:02:00 +0330

    javaScript-1-2-3

---
+++ folder-7-28Dey/script.js
@@ -0,0 +1,40 @@
+
+
+// const airplane = [
+//   ["niloofar", "kosar", "Fatemeh"],
+//   ["ali", "mohsen", "Shirin"],
+//   ["atena", "mahdi", "benyamin"],
+//   ["bani", "mohammad", "sina"],
+//   ["Amir", "Beti", "Pourya"],
+// ];
+
+
+// for (let i = 0 ; i< airplane.length ; i++){
+//     console.log(airplane[i])
+//     for (let j = 0 ; j < airplane[i].length ; j++){
+//         console.log(airplane[i][j])
+//     }
+// }
+
+
+
+
+// const arr = [12,15,26,36,32,86,33,12,64,75]
+
+
+// let newArr = arr.some(ele => ele > 75)
+// let newArr2 = arr.every(ele => ele > 75)
+
+// console.log(newArr)
+// console.log(newArr2)
+
+
+setTimeout((ele) => {
+    console.log("Hello")
+},5000 )
+
+
+setInterval(ele => {
+    console.log("Marg bar Kh")
+},1000)
+
