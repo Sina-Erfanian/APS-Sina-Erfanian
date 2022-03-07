84a24c6a187074b3e12a21fabb2f332cc4af5115
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-03-07 14:51:38 +0330

    update

---
+++ folder-25-15Esfand&16Esfand/module.js
@@ -0,0 +1,13 @@
+// const {sum , PI , minus} = require("./script")
+
+
+
+// sum(2,2)
+// console.log(PI);
+// minus(10 , 5)
+
+// console.log(require);
+
+// const math = require("./script")
+
+// console.log(math.PI)
--- folder-25-15Esfand&16Esfand/script.js
+++ folder-25-15Esfand&16Esfand/script.js
@@ -8,7 +8,49 @@
 //     console.log(`path : ${arg}`)
 // }
 
+// let sum = (x,y)=> console.log(x + y);
 
+// let PI = 3.14
 
+// let minus = (x,y) => console.log(x - y);
 
+// module.exports.sum = sum
+// module.exports.PI = PI
+// module.exports.minus = minus
 
+// console.log(module)
+
+const fs = require("fs");
+
+// console.log(fs);
+
+// fs.mkdir("test", {recursive : true}, (err)=> {
+//     if(err) {
+//         console.log("error dary golam")
+//         throw err
+//     }
+// })
+
+// fs.mkdirSync("test-2")
+
+// let name = "project"
+
+// fs.mkdirSync(name, {recursive : true})
+
+// fs.writeFileSync(`${name}/index.html`,"<html></html>")
+// fs.writeFileSync(`${name}/style.css`,"*{padding : 0}")
+// fs.writeFileSync(`${name}/script.js`,"console.log()")
+
+var giveMeAJoke = require("give-me-a-joke");
+var colors = require("colors");
+const snakeNames = require("snake-names");
+const Joke = require("awesome-dev-jokes");
+
+console.log(Joke.getRandomJoke().red);
+
+// let get = snakeNames.random();
+// console.log(get);
+
+// giveMeAJoke.getRandomDadJoke(function (joke) {
+//   console.log(joke.underline.red);
+// });
